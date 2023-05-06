547b925e62b066a0f21c00ce57f7d92eddd60c17
Arendelle <arendelle@outlook.my>
2023-05-06 15:02:31 +0800

    单次仿真模块完成

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* 
+ * 作者：Arendelle
+ * Mail：arendelle@outlook.my
+ * 仓库地址：https://github.com/Arendelle/QueueSimulation
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,19 +35,18 @@ namespace QueueSimulation
         {
             InitializeComponent();
         }
-
+        // 单次仿真数据显示到输出窗口
         private void Display_output(double[,] data)
         {
-            Label_OutputMode.Content = "模型：M/M/m";
             TextBox_Ws.Text = data[0, 0].ToString();
             TextBox_Wq.Text = data[0, 1].ToString();
             TextBox_Wb.Text = data[0, 2].ToString();
             TextBox_Ls.Text = data[0, 3].ToString();
             TextBox_Lq.Text = data[0, 4].ToString();
+            ListBox_Length.Items.Clear();
             for (int i = 5; i < data.GetLength(1); i++) {
-                //ListBox_Length.Items.Add((i - 5).ToString() + data[0, i].ToString());
                 ListBoxItem newItem = new ListBoxItem();
-                newItem.Content = (i - 5).ToString() + " : " + data[0, i].ToString();
+                newItem.Content = (i - 5).ToString() + ": " + data[0, i].ToString();
                 ListBox_Length.Items.Add(newItem);
             }
         }
@@ -71,9 +75,35 @@ namespace QueueSimulation
             MWNumericArray result = (MWNumericArray)QueueCalc.M_M_m((MWArray)s, m, lambda, mu, t);
             double[,] ans = (double[,])result.ToArray();
             Display_output(ans);
-            //Console.WriteLine(ans);
+            Label_OutputMode.Content = "模型：M/M/m";
         }
 
+        private void Button_sim2_Click(object sender, RoutedEventArgs e)
+        {
+            int s = Convert.ToInt32(TextBox_s2.Text);
+            int m = Convert.ToInt32(TextBox_m2.Text);
+            double lambda = Convert.ToDouble(TextBox_lambda2.Text);
+            double d = Convert.ToDouble(TextBox_mu2.Text);
+            int t = Convert.ToInt32(TextBox_simtime2.Text);
+            MWNumericArray result = (MWNumericArray)QueueCalc.M_D_m((MWArray)s, m, lambda, d, t);
+            double[,] ans = (double[,])result.ToArray();
+            Display_output(ans);
+            Label_OutputMode.Content = "模型：M/D/m";
+        }
+        private void Button_sim3_Click(object sender, RoutedEventArgs e)
+        {
+            int s = Convert.ToInt32(TextBox_s2.Text);
+            int m = Convert.ToInt32(TextBox_m2.Text);
+            double lambda = Convert.ToDouble(TextBox_lambda2.Text);
+            double a = Convert.ToDouble(TextBox_a3.Text);
+            double b = Convert.ToDouble(TextBox_b3.Text);
+            int t = Convert.ToInt32(TextBox_simtime2.Text);
+            MWNumericArray result = (MWNumericArray)QueueCalc.M_G_m((MWArray)s, m, lambda, a, b, t);
+            double[,] ans = (double[,])result.ToArray();
+            Display_output(ans);
+            Label_OutputMode.Content = "模型：M/G/m";
+        }
+        // 启动程序时开启DLL加载进程
         private void MainWindow_Load(object sender, RoutedEventArgs e)
         {
             //LoadMatlabMethod();
@@ -85,8 +115,8 @@ namespace QueueSimulation
         private void LoadMatlabMethod()
         {
             Dispatcher.Invoke((new Action(() => { QueueCalc = new MatlabClass(); })));
-            //Thread.Sleep(5000);
             Console.WriteLine("DLL Loaded.");
         }
+
     }
 }
